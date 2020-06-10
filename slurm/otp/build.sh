7374ed23cb5c6f82722969e9a0eb765bde72e260
Sina Masoud-Ansari <sina.masoud.ansari@gmail.com>
2020-06-10 00:00:03 +0000

    sanity checks

--- slurm/otp/build.sh
+++ slurm/otp/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# note opt does not like symlinks
-
 java -Xmx2G -jar otp.jar --basePath "${PWD}" --build "${PWD}/auckland"
 #java -Xmx2G -jar otp.jar --basePath "${PWD}" --build "${PWD}/wellington"
 #java -Xmx2G -jar otp.jar --basePath "${PWD}" --build "${PWD}/christchurch"
