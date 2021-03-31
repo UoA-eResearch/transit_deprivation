c354ee8019239464edc7caf6649edfc32a1dc8de
Sina Masound-Ansari <sina.masoud.ansari@gmail.com>
2021-03-31 03:20:58 +0000

    fotter

--- app/frontend/src/components/App.js
+++ app/frontend/src/components/App.js
@@ -27,6 +27,10 @@ const styles = (theme) => ({
         color: theme.palette.text.secondary,
         background: theme.palette.background.paper
     },
+    footer: {
+        color: theme.palette.text.secondary,
+        marginTop: theme.spacing(2),
+    }
 });
 
 class App extends Component {
@@ -38,10 +42,10 @@ class App extends Component {
                 <Grid item container direction="column" xs={4} spacing={3}>
                     <Grid item>
                         <Paper className={classes.paper}>
-                            <Typography variant="h4" gutterBottom>
+                            <Typography variant="h4">
                                 Transit & Deprivation
                             </Typography>
-                            <Typography variant="body1" style={{whiteSpace: 'pre-line'}}>
+                            <Typography variant="body1" style={{whiteSpace: 'pre-line', marginTop: theme.spacing(2)}}>
                                 {
                                     "This tool visualises the impact of deprivation on accessibility via public transport in Auckland\n\n" +
                                     "Start by selecting a destination data set in the Data panel below. Then use the Destination panel to select a location of interest and the Origin panel to select a starting location\n\n"
@@ -60,6 +64,32 @@ class App extends Component {
                     </Grid>
                 </Grid>
                 <MapPanel />
+                <Grid container item direction="column" spacing={3} alignItems="center" className={classes.footer}>
+                    <Grid item container direction="row" spacing={3} justify="space-around">
+                        <Grid item>
+                            <Typography>University of Auckland</Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography>Victoria University of Wellington</Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography>University of Cantebury</Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography>Ministry of Business, Innovation and Employment</Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid item>
+                        <Typography>
+                            Contact: xyz@auckland.ac.nz
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <Typography>
+                            Favicon "route" by Viktor Fedyuk (Tim P) from the Noun Project
+                        </Typography>
+                    </Grid>
+                </Grid>
             </Grid>
         );
     }
