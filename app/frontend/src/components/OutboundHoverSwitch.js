adaae0de7f0d7356a95b0c9ae591c24380c6d4b4
Sina Masound-Ansari <sina.masoud.ansari@gmail.com>
2021-03-31 01:52:39 +0000

    outbound popup switch

---
+++ app/frontend/src/components/OutboundHoverSwitch.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { withStyles, createMuiTheme} from '@material-ui/core/styles';
+import { Grid, Switch, Typography} from '@material-ui/core';
+import {setShowOutboundHover} from "../store/actions";
+
+const theme = createMuiTheme({
+    palette: {
+        type: "light",
+    },
+});
+
+const styles = (theme) => ({
+
+});
+
+class OutboundHoverSwitch extends Component {
+
+    handleChange = (event, value) => {
+        const {setShowOutboundHover} = this.props;
+        setShowOutboundHover(value);
+    }
+
+    render() {
+        const { classes, show } = this.props;
+
+        return (
+            <Grid container direction="row" spacing={1} alignItems="center">
+                <Grid item>
+                    <Typography style={{paddingTop:0}}>
+                        Show outbound accessibility time series
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Switch
+                        checked={show}
+                        color="primary"
+                        onChange={this.handleChange}
+                        inputProps={{ 'aria-label': 'secondary checkbox' }}
+                    />
+                </Grid>
+            </Grid>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        show: state.showOutboundHover
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return ({
+        setShowOutboundHover: (show) => {dispatch(setShowOutboundHover(show))},
+    });
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(withStyles(styles, {defaultTheme: theme})(OutboundHoverSwitch));
